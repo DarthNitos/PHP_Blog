67e25d3c7305806528bffec6f281c5421cad8986
Nikita Smetanko <Nitos363@gmail.com>
2020-09-15 21:03:29 -0700

    Blog posting website using PHP

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+  define('ROOT_URL', 'http://localhost/sandbox/PHP_Blog/');
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', 'toor');
+  define('DB_NAME', 'phpblog');
+ ?>
---
+++ inc/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP Blog</title>
+    <link rel="stylesheet" href="https://bootswatch.com/4/cerulean/bootstrap.min.css">
+  </head>
+  <body>
+    <?php include('navbar.php'); ?>
